1f9b175bb3e144bc191794680465228d85b0c82d
Mikhail <jegnum@gmail.com>
2022-03-28 05:24:07 +0300

    v1.0

--- ImageTest/ViewController.swift
+++ ImageTest/ViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageViewUp: UIImageView!
+    
+    @IBOutlet weak var imageViewDown: UIImageView!
+    
+    var image = [UIImage(named: "1"), UIImage(named: "2"), UIImage(named: "3"),
+                 UIImage(named: "4"), UIImage(named: "5"), UIImage(named: "6")]
+    
+    var imageSelector = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let tapGestureUp = UITapGestureRecognizer(target: self, action: #selector(ViewController.imageTappedUp(gesture:)))
+        let tapGestureDown = UITapGestureRecognizer(target: self, action: #selector(ViewController.imageTappedDown(gesture:)))
+        
+        imageViewUp.addGestureRecognizer(tapGestureUp)
+        imageViewUp.isUserInteractionEnabled = true
+        imageViewDown.addGestureRecognizer(tapGestureDown)
+        imageViewDown.isUserInteractionEnabled = true
     }
-
-
+    
+    @objc func imageTappedUp(gesture: UIGestureRecognizer) {
+        if (gesture.view as? UIImageView) != nil {
+            if imageSelector <= image.count - 1 {
+                for _ in image {
+                    imageViewDown.image = image[imageSelector]
+                    imageSelector += 1
+                    break
+                }
+            } else {
+                print("Stop")
+            }
+        }
+    }
+    
+    @objc func imageTappedDown(gesture: UIGestureRecognizer) {
+        if (gesture.view as? UIImageView) != nil {
+            if imageSelector <= image.count - 1 {
+                for _ in image {
+                    imageViewUp.image = image[imageSelector]
+                    imageSelector += 1
+                    break
+                }
+            } else {
+                print("Stop")
+            }
+        }
+    }
+    
 }
+
 
