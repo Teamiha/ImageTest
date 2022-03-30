ddd54a94c7225b25f108df3b5bfc7cfed315b619
Mikhail <jegnum@gmail.com>
2022-03-30 20:27:58 +0300

    add dictionary

--- ImageTest/ViewController.swift
+++ ImageTest/ViewController.swift
@@ -14,12 +14,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageViewDown: UIImageView!
     
     var image = [UIImage(named: "1"), UIImage(named: "2"), UIImage(named: "3"),
-                 UIImage(named: "4"), UIImage(named: "5"), UIImage(named: "6")]
+                 UIImage(named: "4"), UIImage(named: "5"), UIImage(named: "6"),
+                 UIImage(named: "7"), UIImage(named: "8"), UIImage(named: "9"),
+                 UIImage(named: "10")
+                ]
+    
+    var testImage: [String: String] =
+                   ["1": "The Machinist", "2": "Mortal Engines",
+                    "3": "The Sorcerer's Apprentice", "4": "Tenet",
+                    "5": "Kick Ass", "6": "Peacemaker",
+                    "7": "Vikings", "8": "Brightburn",
+                    "9": "Reacher", "10": "Disenchantment",
+                   
+                   ]
+    
+    
     
     var imageSelector = 2
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageViewUp.image = UIImage(named: testImage["1"] ?? "Error")
+        
+        
+        
+        
+        
+        
+        
         let tapGestureUp = UITapGestureRecognizer(target: self, action: #selector(ViewController.imageTappedUp(gesture:)))
         let tapGestureDown = UITapGestureRecognizer(target: self, action: #selector(ViewController.imageTappedDown(gesture:)))
         
